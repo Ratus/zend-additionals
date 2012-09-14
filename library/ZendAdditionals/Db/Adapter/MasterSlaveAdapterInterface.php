34e7c04acf7e2afceaaac7db6f5fb3917a47a150
bboer <bartdeboer86@gmail.com>
2012-09-14 10:49:49 +0200

    added master-slave-adapter to zend-additionals

---
+++ library/ZendAdditionals/Db/Adapter/MasterSlaveAdapterInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ZendAdditionals\Db\Adapter;
+
+interface MasterSlaveAdapterInterface
+{
+    /**
+     * @return Zend\Db\Adapter
+     */
+    public function getSlaveAdapter();
+}
