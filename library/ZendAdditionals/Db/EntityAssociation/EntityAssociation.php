8b3c17e6298a4bfdd5bb0297bde6deb6cfa26136
bboer <bartdeboer86@gmail.com>
2012-10-04 15:48:26 +0200

    tabs to spaces

--- library/ZendAdditionals/Db/EntityAssociation/EntityAssociation.php
+++ library/ZendAdditionals/Db/EntityAssociation/EntityAssociation.php
@@ -9,234 +9,234 @@ use Zend\Db\Sql\Predicate\Predicate;
 
 class EntityAssociation
 {
-	/**
-	 * @var string
-	 */
-	protected $entityIdentifier;
+    /**
+     * @var string
+     */
+    protected $entityIdentifier;
 
-	/**
-	 * @var string
-	 */
-	protected $parentMapperServiceName;
+    /**
+     * @var string
+     */
+    protected $parentMapperServiceName;
 
-	/**
-	 * @var string
-	 */
-	protected $mapperServiceName;
+    /**
+     * @var string
+     */
+    protected $mapperServiceName;
 
-	/**
-	 * @var \Zend\ServiceManager\ServiceManager
-	 */
-	protected $serviceManager;
+    /**
+     * @var \Zend\ServiceManager\ServiceManager
+     */
+    protected $serviceManager;
 
-	/**
-	 * @var string
-	 */
-	protected $generatedAlias;
+    /**
+     * @var string
+     */
+    protected $generatedAlias;
 
-	/**
-	 * @var int
-	 */
-	protected static $aliasSuffixCount = 0;
+    /**
+     * @var int
+     */
+    protected static $aliasSuffixCount = 0;
 
-	protected $parentAlias;
+    protected $parentAlias;
 
-	protected $required;
+    protected $required;
 
-	/** @var Predicate */
-	protected $predicate;
+    /** @var Predicate */
+    protected $predicate;
 
-	protected $tablePrefix;
+    protected $tablePrefix;
 
-	/**
-	 *
-	 * @param type $entityIdentifier
-	 * @param string $parentMapperServiceName
-	 * @param string $mapperServiceName
-	 */
-	public function __construct(
-		$entityIdentifier,
-		$parentMapperServiceName,
-		$mapperServiceName
-	) {
-		static::$aliasSuffixCount++;
-		$this->entityIdentifier        = $entityIdentifier;
-		$this->parentMapperServiceName = $parentMapperServiceName;
-		$this->mapperServiceName       = $mapperServiceName;
-		$this->generatedAlias          = $entityIdentifier . '_' . static::$aliasSuffixCount;
-		$this->predicate               = new Predicate();
-	}
+    /**
+     *
+     * @param type $entityIdentifier
+     * @param string $parentMapperServiceName
+     * @param string $mapperServiceName
+     */
+    public function __construct(
+        $entityIdentifier,
+        $parentMapperServiceName,
+        $mapperServiceName
+    ) {
+        static::$aliasSuffixCount++;
+        $this->entityIdentifier        = $entityIdentifier;
+        $this->parentMapperServiceName = $parentMapperServiceName;
+        $this->mapperServiceName       = $mapperServiceName;
+        $this->generatedAlias          = $entityIdentifier . '_' . static::$aliasSuffixCount;
+        $this->predicate               = new Predicate();
+    }
 
-	public function getParentAlias()
-	{
-		return $this->parentAlias;
-	}
+    public function getParentAlias()
+    {
+        return $this->parentAlias;
+    }
 
-	public function setParentAlias($parentAlias)
-	{
-		$this->parentAlias = $parentAlias;
-		return $this;
-	}
+    public function setParentAlias($parentAlias)
+    {
+        $this->parentAlias = $parentAlias;
+        return $this;
+    }
 
 
-	public function getEntityIdentifier()
-	{
-		return $this->entityIdentifier;
-	}
+    public function getEntityIdentifier()
+    {
+        return $this->entityIdentifier;
+    }
 
-	public function setServiceManager($serviceManager)
-	{
-		$this->serviceManager = $serviceManager;
-	}
+    public function setServiceManager($serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+    }
 
-	public function getServiceManager()
-	{
-		return $this->serviceManager;
-	}
+    public function getServiceManager()
+    {
+        return $this->serviceManager;
+    }
 
-	public function getRequiredByAssociation()
-	{
-		return $this->requiredByAssociation;
-	}
+    public function getRequiredByAssociation()
+    {
+        return $this->requiredByAssociation;
+    }
 
-	public function getAssociationRequiredRelation()
-	{
-		return $this->associationRequiredRelation;
-	}
+    public function getAssociationRequiredRelation()
+    {
+        return $this->associationRequiredRelation;
+    }
 
-	public function getAlias()
-	{
-		return $this->generatedAlias;
-	}
+    public function getAlias()
+    {
+        return $this->generatedAlias;
+    }
 
-	public function getTableName()
-	{
-		return $this->getTablePrefix().$this->getMapper()->getTableName();
-	}
+    public function getTableName()
+    {
+        return $this->getTablePrefix().$this->getMapper()->getTableName();
+    }
 
-	public function setTablePrefix($tablePrefix)
-	{
-		$this->tablePrefix = $tablePrefix;
-		return $this;
-	}
+    public function setTablePrefix($tablePrefix)
+    {
+        $this->tablePrefix = $tablePrefix;
+        return $this;
+    }
 
-	public function getTablePrefix()
-	{
-		return $this->tablePrefix;
-	}
+    public function getTablePrefix()
+    {
+        return $this->tablePrefix;
+    }
 
-	public function getJoinTable()
-	{
-		return array($this->getAlias() => $this->getTableName());
-	}
+    public function getJoinTable()
+    {
+        return array($this->getAlias() => $this->getTableName());
+    }
 
-	/**
-	* @var Predicate
-	*/
-	public function getPredicate()
-	{
-		return $this->predicate;
-	}
+    /**
+    * @var Predicate
+    */
+    public function getPredicate()
+    {
+        return $this->predicate;
+    }
 
-	public function getRelation()
-	{
-		return $this->getParentMapper()->getRelation($this->mapperServiceName, $this->entityIdentifier);
-	}
+    public function getRelation()
+    {
+        return $this->getParentMapper()->getRelation($this->mapperServiceName, $this->entityIdentifier);
+    }
 
-	/**
-	 * @return Select::JOIN_INNER or Select::JOIN_LEFT
-	 */
-	public function getJoinType()
-	{
-		if ($this->getRequired()) {
-			return \Zend\Db\Sql\Select::JOIN_INNER;
-		}
-		return \Zend\Db\Sql\Select::JOIN_LEFT;
-	}
+    /**
+     * @return Select::JOIN_INNER or Select::JOIN_LEFT
+     */
+    public function getJoinType()
+    {
+        if ($this->getRequired()) {
+            return \Zend\Db\Sql\Select::JOIN_INNER;
+        }
+        return \Zend\Db\Sql\Select::JOIN_LEFT;
+    }
 
-	public function setRequired($required)
-	{
-		$this->required = $required;
-	}
+    public function setRequired($required)
+    {
+        $this->required = $required;
+    }
 
-	public function getRequired()
-	{
-		$relation =  $this->getParentMapper()->getRelation($this->mapperServiceName, $this->entityIdentifier);
-		if (!is_null($this->required)) {
-			return $this->required;
-		}
-		return $relation['required'];
-	}
+    public function getRequired()
+    {
+        $relation =  $this->getParentMapper()->getRelation($this->mapperServiceName, $this->entityIdentifier);
+        if (!is_null($this->required)) {
+            return $this->required;
+        }
+        return $relation['required'];
+    }
 
-	/**
-	 *
-	 * @return array
-	 * @throws \UnexpectedValueException
-	 */
-	public function getJoinColumns($aliasPrefix = null)
-	{
-		$hydrator = $this->getMapper()->getHydrator();
-		if (!($hydrator instanceof ObservableClassMethods)) {
-			throw new \UnexpectedValueException('EntityAssociation expects the mapper to have an ObservableClassMethods hydrator.');
-		}
-		$excludedColumns = $this->getMapper()->getEntityAssociationColumns();
-		$columns = $hydrator->extract($this->getMapper()->getEntityPrototype());
-		foreach ($columns as $key => & $column) {
-			if (
-				array_search($key, $excludedColumns) !== false
-			) {
-				unset($columns[$key]);
-				continue;
-			}
-			$column = $this->getAlias($aliasPrefix) . '__' . $key;
-		}
+    /**
+     *
+     * @return array
+     * @throws \UnexpectedValueException
+     */
+    public function getJoinColumns($aliasPrefix = null)
+    {
+        $hydrator = $this->getMapper()->getHydrator();
+        if (!($hydrator instanceof ObservableClassMethods)) {
+            throw new \UnexpectedValueException('EntityAssociation expects the mapper to have an ObservableClassMethods hydrator.');
+        }
+        $excludedColumns = $this->getMapper()->getEntityAssociationColumns();
+        $columns = $hydrator->extract($this->getMapper()->getEntityPrototype());
+        foreach ($columns as $key => & $column) {
+            if (
+                array_search($key, $excludedColumns) !== false
+            ) {
+                unset($columns[$key]);
+                continue;
+            }
+            $column = $this->getAlias($aliasPrefix) . '__' . $key;
+        }
 
-		return array_flip($columns);
-	}
+        return array_flip($columns);
+    }
 
-	public function getPrototype()
-	{
-		return $this->getMapper()->getEntityPrototype();
-	}
+    public function getPrototype()
+    {
+        return $this->getMapper()->getEntityPrototype();
+    }
 
-	public function getAssociatedEntity($baseEntity)
-	{
-		$formatter = new \Zend\Filter\Word\UnderscoreToCamelCase;
-		if (!is_object($baseEntity)) {
-			// throw
-		}
-		$getAssociatedEntityMethod = 'get' . $formatter($this->alias);
-		if (!method_exists($baseEntity, $getAssociatedEntityMethod)) {
-			// throw
-		}
+    public function getAssociatedEntity($baseEntity)
+    {
+        $formatter = new \Zend\Filter\Word\UnderscoreToCamelCase;
+        if (!is_object($baseEntity)) {
+            // throw
+        }
+        $getAssociatedEntityMethod = 'get' . $formatter($this->alias);
+        if (!method_exists($baseEntity, $getAssociatedEntityMethod)) {
+            // throw
+        }
 
-		return $baseEntity->$getAssociatedEntityMethod();
-	}
+        return $baseEntity->$getAssociatedEntityMethod();
+    }
 
-	/**
-	 * @return AbstractMapper
-	 * @throws \InvalidArgumentException
-	 */
-	public function getParentMapper()
-	{
-		$mapper = $this->getServiceManager()->get($this->parentMapperServiceName);
-		if (!is_object($mapper) || !($mapper instanceof AbstractMapper)) {
-			throw new \InvalidArgumentException('Mapper must be an instance of AbstractMapper');
-		}
-		return $mapper;
-	}
+    /**
+     * @return AbstractMapper
+     * @throws \InvalidArgumentException
+     */
+    public function getParentMapper()
+    {
+        $mapper = $this->getServiceManager()->get($this->parentMapperServiceName);
+        if (!is_object($mapper) || !($mapper instanceof AbstractMapper)) {
+            throw new \InvalidArgumentException('Mapper must be an instance of AbstractMapper');
+        }
+        return $mapper;
+    }
 
-	/**
-	 * @return AbstractMapper
-	 * @throws \InvalidArgumentException
-	 */
-	public function getMapper()
-	{
-		$mapper = $this->getServiceManager()->get($this->mapperServiceName);
-		if (!is_object($mapper) || !($mapper instanceof AbstractMapper)) {
-			throw new \InvalidArgumentException('Mapper must be an instance of AbstractMapper');
-		}
-		return $mapper;
-	}
+    /**
+     * @return AbstractMapper
+     * @throws \InvalidArgumentException
+     */
+    public function getMapper()
+    {
+        $mapper = $this->getServiceManager()->get($this->mapperServiceName);
+        if (!is_object($mapper) || !($mapper instanceof AbstractMapper)) {
+            throw new \InvalidArgumentException('Mapper must be an instance of AbstractMapper');
+        }
+        return $mapper;
+    }
 }
 
--- library/ZendAdditionals/Http/PostProcessor/Xml.php
+++ library/ZendAdditionals/Http/PostProcessor/Xml.php
@@ -9,52 +9,52 @@ class Xml extends AbstractPostProcessor
 {
     public function process()
     {
-    	$result = $this->toXML($this->getVariables());
+        $result = $this->toXML($this->getVariables());
 
-    	$this->getResponse()->setContent($result);
+        $this->getResponse()->setContent($result);
 
-    	$headers = $this->getResponse()->getHeaders();
-    	$headers->addHeaderLine('Content-Type', 'text/xml');
-    	$this->getResponse()->setHeaders($headers);
+        $headers = $this->getResponse()->getHeaders();
+        $headers->addHeaderLine('Content-Type', 'text/xml');
+        $this->getResponse()->setHeaders($headers);
     }
 
     public function toXml($data, $rootNodeName = 'data', $xml=null)
     {
-    	// turn off compatibility mode as simple xml throws a wobbly if you don't.
-    	if (ini_get('zend.ze1_compatibility_mode') == 1) {
-    		ini_set ('zend.ze1_compatibility_mode', 0);
-    	}
+        // turn off compatibility mode as simple xml throws a wobbly if you don't.
+        if (ini_get('zend.ze1_compatibility_mode') == 1) {
+            ini_set ('zend.ze1_compatibility_mode', 0);
+        }
 
-    	if ($xml == null) {
-    		$xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
-    	}
+        if ($xml == null) {
+            $xml = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$rootNodeName />");
+        }
 
-    	// loop through the data passed in.
-    	foreach($data as $key => $value)
-    	{
-    		// no numeric keys in our xml please!
-    		if (is_numeric($key)) {
-    			// make string key...
-    			$key = sprintf("element_%d", $key);
-    		}
+        // loop through the data passed in.
+        foreach($data as $key => $value)
+        {
+            // no numeric keys in our xml please!
+            if (is_numeric($key)) {
+                // make string key...
+                $key = sprintf("element_%d", $key);
+            }
 
-    		// replace anything not alpha numeric
-    		$key = preg_replace('/[^a-zA-Z0-9_]/i', '', $key);
+            // replace anything not alpha numeric
+            $key = preg_replace('/[^a-zA-Z0-9_]/i', '', $key);
 
-    		// if there is another array found recrusively call this function
-    		if (is_array($value) || is_object($value)) {
-    			$node = $xml->addChild($key);
-    			// recrusive call.
-    			$this->toXml($value, $rootNodeName, $node);
-    		} else {
-    			// add single node.
+            // if there is another array found recrusively call this function
+            if (is_array($value) || is_object($value)) {
+                $node = $xml->addChild($key);
+                // recrusive call.
+                $this->toXml($value, $rootNodeName, $node);
+            } else {
+                // add single node.
                 $value = htmlentities($value);
-    			$xml->addChild($key,$value);
-    		}
+                $xml->addChild($key,$value);
+            }
 
-    	}
-    	// pass back as string. or simple xml object if you want!
-    	return $xml->asXML();
+        }
+        // pass back as string. or simple xml object if you want!
+        return $xml->asXML();
     }
 }
 
