579d1747bf323973e34bf75b2c58161cbf4d62e4
Dennis Seijts <dennisseijts@ratus.nl>
2013-01-10 14:52:24 +0100

    replaced get_class() with get_called_class(). It returns the same, but doesn't depend on an argument

--- library/ZendAdditionals/Mvc/Controller/AbstractRestfulController.php
+++ library/ZendAdditionals/Mvc/Controller/AbstractRestfulController.php
@@ -218,7 +218,7 @@ abstract class AbstractRestfulController extends AbstractController
     protected function createParentFilter(array $parent, array $filter = null)
     {
         throw new NotImplementedException(
-            'The method ' . __METHOD__ . ' must be implemented by class ' . get_class($this)
+            'The method ' . __METHOD__ . ' must be implemented by class ' . get_called_class()
         );
     }
 
@@ -231,7 +231,7 @@ abstract class AbstractRestfulController extends AbstractController
     protected function getUniqueIdentifier()
     {
         throw new NotImplementedException(
-            'The method ' . __METHOD__ . ' must be implemented by class ' . get_class($this)
+            'The method ' . __METHOD__ . ' must be implemented by class ' . get_called_class()
         );
     }
 
