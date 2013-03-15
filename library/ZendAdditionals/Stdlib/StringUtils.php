9454008fd0be697863a75d2099ab98f88697ba03
bboer <bartdeboer86@gmail.com>
2013-03-15 07:12:14 +0100

    Some useful features

--- library/ZendAdditionals/Stdlib/StringUtils.php
+++ library/ZendAdditionals/Stdlib/StringUtils.php
@@ -15,4 +15,52 @@ class StringUtils extends \Zend\Stdlib\StringUtils
         $underscored = strtolower($underscored);
         return preg_replace('/_(.?)/e', "strtoupper('$1')", $underscored);
     }
+
+    /**
+     * Check value to find if it was serialized.
+     *
+     * If $data is not an string, then returned value will always be false.
+     * Serialized data is always a string.
+     *
+     * @param mixed $data Value to check to see if was serialized.
+     *
+     * @return bool false if not serialized and true if it was.
+     */
+    public static function isSerialized($data)
+    {
+        // if it isn't a string, it isn't serialized
+        if (!is_string($data)) {
+            return false;
+        }
+        $data = trim($data);
+        if ('N;' == $data) {
+            return true;
+        }
+        $length = strlen($data);
+        if ($length < 4) {
+            return false;
+        }
+        if (':' !== $data[1]) {
+            return false;
+        }
+        $lastc = $data[$length-1];
+        if (';' !== $lastc && '}' !== $lastc) {
+            return false;
+        }
+        $token = $data[0];
+        switch ($token) {
+            case 's':
+                if ('"' !== $data[$length-2]) {
+                    return false;
+                }
+            case 'a':
+            case 'O':
+                return (bool) preg_match("/^{$token}:[0-9]+:/s", $data);
+            case 'b':
+            case 'i':
+            case 'd':
+                return (bool) preg_match("/^{$token}:[0-9.E-]+;\$/", $data);
+        }
+        return false;
+    }
 }
