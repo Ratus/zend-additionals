ad27693609486b4e58b68380f3a2545d987987f0
bboer <bartdeboer86@gmail.com>
2013-07-30 14:17:00 +0200

    Pall the parent resolve to be able to profit from parent optimizations

--- library/ZendAdditionals/AssetManager/Resolver/CollectionResolver.php
+++ library/ZendAdditionals/AssetManager/Resolver/CollectionResolver.php
@@ -12,53 +12,14 @@ class CollectionResolver extends \AssetManager\Resolver\CollectionResolver
      */
     public function resolve($name)
     {
-        if (!isset($this->collections[$name])) {
+        $result = parent::resolve($name);
+        if (!($result instanceof \Assetic\Asset\AssetCollection)) {
             return null;
         }
-
-        if (!is_array($this->collections[$name])) {
-            throw new Exception\RuntimeException(
-                "Collection with name $name is not an an array."
-            );
-        }
-
         $collection = new AssetCollection;
-        $mimeType   = null;
-
-        foreach ($this->collections[$name] as $asset) {
-
-            if (!is_string($asset)) {
-                throw new Exception\RuntimeException(
-                    'Asset should be of type string. got ' . gettype($asset)
-                );
-            }
-            if (null === ($res = $this->getAggregateResolver()->resolve($asset))) {
-                throw new Exception\RuntimeException("Asset '$asset' could not be found.");
-            }
-
-            if (!$res instanceof AssetInterface) {
-                throw new Exception\RuntimeException(
-                    "Asset '$asset' does not implement Assetic\\Asset\\AssetInterface."
-                );
-            }
-
-            if (null !== $mimeType && $res->mimetype !== $mimeType) {
-                throw new Exception\RuntimeException(sprintf(
-                    'Asset "%s" from collection "%s" doesn\'t have the expected mime-type "%s".',
-                    $asset,
-                    $name,
-                    $mimeType
-                ));
-            }
-
-            $mimeType = $res->mimetype;
-
-            $this->getAssetFilterManager()->setFilters($asset, $res);
-
-            $collection->add($res);
-        }
-
-        $collection->mimetype = $mimeType;
+        
+        $collection->mimetype = $result->mimetype;
+        $collection->setAll($result->all());
 
         return $collection;
     }
