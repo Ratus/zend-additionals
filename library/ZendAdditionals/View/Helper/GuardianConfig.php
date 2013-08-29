66b6193fb4bd77e52aab5516c23b401b66c43883
Lekha <lekha@bridge-india.in>
2013-08-29 11:53:37 +0530

    added helper for guardian config

---
+++ library/ZendAdditionals/View/Helper/GuardianConfig.php
@@ -0,0 +1,42 @@
+<?php
+namespace ZendAdditionals\View\Helper;
+
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\View\Helper\AbstractHelper;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Helper for guardian config
+ */
+class GuardianConfig extends AbstractHelper implements ServiceLocatorAwareInterface
+{
+    use \ZendAdditionals\Config\ConfigExtensionTrait;
+    use \Zend\ServiceManager\ServiceLocatorAwareTrait;
+
+    protected $pluginManager;
+
+    /**
+     * Get service locator
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        $locator = $this->serviceLocator;
+        if ($locator instanceof \Zend\ServiceManager\AbstractPluginManager) {
+            $this->pluginManager = $locator;
+            return $locator->getServiceLocator();
+        }
+        return $locator;
+    }
+
+    /**
+     * Get guardian config
+     *
+     * @return guardian config
+    */
+    public function __invoke() {
+        $config = $this->getServiceLocator()->get('config');
+        return $config['guardian'];
+    }
+}
